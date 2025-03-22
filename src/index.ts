6409c488499a34b20bf1b34b680f9496ccaa48bd
Bilal Javaid <109686186+Bilal3940@users.noreply.github.com>
2025-03-23 02:33:50 +0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,7 @@ import { server } from './bootstrap'
 import './routes'
 config()
 
-const { PORT } = process.env || 3004
- 
+const port = process.env.PORT || 4001;
 server.listen(PORT, async () => {
   console.log(`Server raised in port: ${PORT}`)
 })
