98e1d42769a2680ec2b1dddae217a04f6e25b65a
bilal3940 <bilalj3940@gmail.com>
2025-04-08 13:04:20 +0500

    updates

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { server } from './bootstrap'
 import './routes'
 config()
  
-const port = Number(process.env.PORT) || 3001; // Convert to number
+const port = Number(process.env.PORT) || 8001; // Convert to number
 
 server.listen(port, () => {
     console.log(`Server raised in port: ${port}`);
